ee8123cc5b0a0fa1145fbe94a0af1b8b0a2c5573
martinizoliwkami <martyna.gryglas22@gmail.com>
2024-10-15 11:54:58 +0200

    Add files via upload

---
+++ zadanie2.R
@@ -0,0 +1,35 @@
+**Zainstaluj pakiet `ggplot2`.**
+  
+  ``` r
+install.packages("ggplot2")
+```
+
+2.  **Załaduj pakiet.**
+  
+  ``` r
+library(ggplot2)
+# spróbowałam to zrobić z komendą install.packages("ggplot2", dependencies = TRUE)
+
+```
+
+3.  **Sprawdź dokumentację pakietu.**
+  
+  ``` r
+?ggplot2
+```
+
+4.  **Zainstaluj i załaduj** dodatkowy pakiet, który Cię zainteresuje. Listę pakietów w repozytorium CRAN wraz z opisami znajdziesz tutaj: <https://cran.r-project.org/web/packages/available_packages_by_name.html>
+  
+  ``` r
+install.packages("MDDC")
+library(MDDC)
+```
+
+5.  **Napisz krótki opis** w komentarzu.
+
+``` r
+# Pakiet MDDC - Zmodyfikowany algorytm wykrywania odchylających się komórek w ramach nadzoru nad bezpieczeństwem farmakoterapii.
+Wykorzystywałam go w pracy inżynierskiej, aby sprawdzić wyniki hodowlii. 
+```
+
+6.  **Zapisz skrypt** jako `zadanie2.R` i prześlij go do swojego repozytorium.
