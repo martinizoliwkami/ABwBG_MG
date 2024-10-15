b3875ad58ed2f996904a6bddd9a39e83d65fa52c
martinizoliwkami <martyna.gryglas22@gmail.com>
2024-10-15 11:01:09 +0200

    Add files via upload

---
+++ zadanie1.R
@@ -0,0 +1,27 @@
+## Zadanie 1: Zapoznanie się z RStudio
+
+**Cel:** Poznanie interfejsu i podstawowych funkcji RStudio.
+
+### Instrukcje i rozwiązanie:
+
+1.  **Uruchom RStudio** na swoim komputerze.
+
+2.  **Zapoznaj się z interfejsem użytkownika:**
+  
+  3.  **Stwórz nowy skrypt R:**
+  
+  4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
+# Source (edytor) słuzy do pisania skryptów, a funkcje nie są wykonywane automatycznie.
+# Console (Konsola) pozwala na wykonanie skryptów.
+# Environment (środowisko) mówi nam o tym, jakie mamy przypisane dane/wartości/nazwy
+# Output panel (wyjściowy) gdzie mamy pakiety, pliki i pomoc
+
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+# Pastel on dark -> wybrałam w Tools i potem Global options
+
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+# Ponieważ ma szare tło i delikatniejsze barwy, więc z doświadczenia można z nim dłużej wytrzymać.
+
+7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
+
+8.  **Prześlij plik do swojego repozytorium na GitHub**.
